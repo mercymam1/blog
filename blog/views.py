8ec0001808bdbff2a596f348d2eb38bca64381df
mercy <mercymamedu16@gmail.com>
2020-08-09 13:16:38 +0100

    blogging

--- blog/views.py
+++ blog/views.py
@@ -5,4 +5,5 @@ def	post_list(request):
 	posts	=	Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')				
 	return	render(request,	'blog/post_list.html',	{'posts':	posts})
 def blogging(request):
-	return render(request, 'blog/blogging.html', {})
+	posts	=	Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+	return render(request, 'blog/blogging.html', {'posts':	posts})
