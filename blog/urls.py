a1a9e04c44ecb638034d1d677523fb7c27def786
mercy <mercymamedu16@gmail.com>
2020-08-09 14:09:29 +0100

    adding view and template

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns	=	[				
 	path('',	views.post_list,	name='post_list'),
 	path('blogging/', views.blogging , name='blogging'), 
+	path('post/<int:pk>/',	views.post_detail,	name='post_detail'), 
 ]
 urlpatterns += staticfiles_urlpatterns()
